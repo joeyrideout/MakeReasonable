fa98d037d7bdb8cdb65bc7f2cce6d19d6dfb4734
Joey Rideout <jrideout@transitlabs.net>
2015-03-19 11:33:03 -0400

    Adding grm alias.

--- makereasonable.sh
+++ makereasonable.sh
@@ -15,16 +15,32 @@ echo '"\e[B":history-searc-forward' >> ~/.inputrc
 # export HISTFILE=~/.bash_eternal_history
 # PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
 
-# Add my custom bash aliases.
+# My custom bash aliases
+alias g='git status'
+alias gs='git status'
+alias gl='git log'
+alias gb='git branch'
+alias gd='git diff'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias b='cd -'
+alias v='vim'
+alias c='clear' Add my custom bash aliases.
 echo "alias g='git status'" >> ~/.bashrc
 echo "alias gs='git status'" >> ~/.bashrc
 echo "alias gl='git log'" >> ~/.bashrc
+echo "alias gd='git diff'" >> ~/.bashrc
 echo "alias gb='git branch'" >> ~/.bashrc
-echo "alias cd ...='cd ../..'" >> ~/.bashrc
-echo "alias cd ...='cd ../../..'" >> ~/.bashrc
+echo "alias ..='cd ..'" >> ~/.bashrc
+echo "alias ...='cd ../..'" >> ~/.bashrc
+echo "alias ....='cd ../../..'" >> ~/.bashrc
 echo "alias b='cd -'" >> ~/.bashrc
 echo "alias v='vim'" >> ~/.bashrc
 echo "alias c='clear'" >> ~/.bashrc
+echo "alias a='ack-grep'" >> ~/.bashrc
+echo "alias ack='ack-grep'" >> ~/.bashrc
+echo "alias grm='git diff --diff-filter=D --name-only -z | xargs -0 git rm'" >> ~/.bashrc
 
 # Make sure Vim is installed.
 sudo apt-get install vim
@@ -39,9 +55,9 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Install pathogen plugins.
 cd ~/.vim/bundle
-git clone https://github.com/nvie/vim-flake8.git
-git clone git://github.com/davidhalter/jedi-vim.git
-git clone https://github.com/mattn/emmet-vim.git
+git clone https://github.com/nvie/vim-flake8.git  # PEP 8
+git clone git://github.com/davidhalter/jedi-vim.git  # Python code-completion
+git clone https://github.com/mattn/emmet-vim.git  # Fast HTML eiditing
 
 # Install ack-grep.
 sudo apt-get install ack-grep
